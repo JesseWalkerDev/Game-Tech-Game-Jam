6e0c9d614275f9bb3a517681174be1da4deabaf3
Jesse <140586222+JesseWalkerDev@users.noreply.github.com>
2024-12-03 12:12:02 -0500

    Added jumping and improved player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public Rigidbody2D rBody;
+	public Rigidbody2D rigidBody;
 	
-	public float acceleration = 20;
+	public float acceleration = 0.6f;
+	public float maxSpeed = 6f;
+	public float jumpForce = 8f;
+	public float groundDrag = 0.92f;
+	public float gravityForce = 25f;
+	public int maxJumpTime = 15;
+	
+	private int jumpTime = 0;
+	private bool jumping = false;
+	private bool grounded = false;
+	
 	
 	// Start is called before the first frame update
 	void Start()
@@ -15,8 +25,48 @@ public class PlayerController : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
-		rBody.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * acceleration, rBody.velocity.y));
+		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		
+		// Horizontal movement
+		rigidBody.velocity = new Vector2(
+			Mathf.Clamp(rigidBody.velocity.x + horizontalInput * acceleration, -maxSpeed, maxSpeed),
+			rigidBody.velocity.y);
+		
+		if (grounded && (horizontalInput == 0f || Mathf.Sign(horizontalInput) != Mathf.Sign(rigidBody.velocity.x)))
+			// Drag if no horizontal input
+			rigidBody.velocity = new Vector2(rigidBody.velocity.x * groundDrag, rigidBody.velocity.y);
+		
+		// Jump
+		if (Input.GetKey(KeyCode.Space) && (grounded || jumping))
+		{
+			rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+			if (grounded)
+				jumpTime = 0;
+			else
+				jumpTime ++;
+			jumping = jumpTime < maxJumpTime;
+		}
+		else
+			jumping = false;
+		
+		// Gravity
+		rigidBody.AddForce(Vector2.down * gravityForce);
+	}
+	
+	void OnCollisionStay2D(Collision2D collision)
+    {
+        grounded = false;
+		foreach (ContactPoint2D contact in collision.contacts)
+        {
+			if (contact.normal.y >= 0.5)
+                grounded = true;
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		grounded = false;
 	}
 }
